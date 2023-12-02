b913e6647bba9dea12cf559679415482fa9b03bd
baonguyen132 <118587919+baonguyen132@users.noreply.github.com>
2023-12-02 23:18:49 +0700

    update display system's employee

--- app/Http/Controllers/Admin/EmployeerConttroler.php
+++ app/Http/Controllers/Admin/EmployeerConttroler.php
@@ -24,7 +24,7 @@ class EmployeerConttroler extends Controller
     {
 
         $page = "index";
-        $data = User::where("status", "<>", 5)->where("status", "<>", 50)->get();
+        $data = User::where("status", "<>", 5)->where("status", "<>", 50)->where("status", "<>", 0)->get();
 
         $user = Auth::user();
         return view("admin.main.layout.register", compact("page", "data", "user"));
